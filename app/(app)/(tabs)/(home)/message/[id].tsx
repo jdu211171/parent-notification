cebaaeaf14b9ce9600926628f913e794927ebc1b
jdu211171 <211171m@jdu.uz>
2024-06-18 11:33:20 +0500

    dynamic page shows message

--- app/(app)/(tabs)/(home)/message/[id].tsx
+++ app/(app)/(tabs)/(home)/message/[id].tsx
@@ -1,20 +1,97 @@
-import { useLocalSearchParams } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import {useLocalSearchParams} from 'expo-router';
+import {StyleSheet} from 'react-native';
+import {ThemedView} from "@/components/ThemedView";
+import {ThemedText} from "@/components/ThemedText";
+import {useSQLiteContext} from "expo-sqlite";
+import React, {useEffect, useState} from "react";
+
+interface Message {
+  content: string;
+  date: string;
+  group_name: string;
+  id: number;
+  image: string;
+  priority: string;
+  read_status: number;
+  read_time: string | null;
+  sent_status: number;
+  sent_time: string | null;
+  student_id: number;
+  title: string;
+}
 
 export default function DetailsScreen() {
-  const { id } = useLocalSearchParams();
+  const {id} = useLocalSearchParams();
+  const db = useSQLiteContext();
+  const [message, setMessage] = useState<Message | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const result = await db.getFirstAsync('SELECT * FROM message WHERE id = ?', [Number(id)]);
+      setMessage(result as Message);
+    };
+
+    fetchMessage()
+        .then(() => setLoading(false));
+  }, [id]);
+
+  useEffect(() => {
+    console.log(message);
+  }, [message]);
+
+  if (loading && !message) {
+    return <ThemedText>Loading...</ThemedText>;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Details of user {id} </Text>
-    </View>
+      <ThemedView style={styles.container}>
+        <ThemedView style={styles.titleRow}>
+          <ThemedText type='default'>
+            {message?.title}
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.dateRow}>
+          <ThemedText style={styles.dateText}>{message?.date}</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.descriptionRow}>
+          <ThemedText>{message?.content}</ThemedText>
+        </ThemedView>
+      </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 10,
+  },
+  titleRow: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  iconContainer: {
+    padding: 5,
+  },
+  dateRow: {
+    flexDirection: 'row',
     alignItems: 'center',
+    marginTop: 5,
+  },
+  dateText: {
+    fontSize: 12,
+    fontWeight: '300',
+  },
+  descriptionRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  readMoreButton: {
+    marginTop: 5,
+  },
+  readMoreText: {
+    color: '#059669',
+    fontWeight: '600',
   },
 });
--- components/card.tsx
+++ components/card.tsx
@@ -1,78 +1,87 @@
-import React from 'react';
-import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, StyleSheet, Pressable} from 'react-native';
 import {HelloWave} from "@/components/HelloWave";
 import {ThemedText} from "@/components/ThemedText";
 import {ThemedView} from './ThemedView';
-import {Link} from "expo-router";
+import {Link, useRouter} from "expo-router";
 
-interface Message {
-  title: string;
-  date?: string;
-  description: string;
-  viewed_at?: string;
+export interface Message {
+    id: number;
+    title: string;
+    date?: string;
+    description: string;
+    viewed_at?: string;
 }
 
-const Card = ({ message }: { message: Message }) => {
-  return (
-    <ThemedView style={styles.container}>
-      <ThemedView style={styles.titleRow}>
-        {message.viewed_at !== '' ? null : (
-          <ThemedView style={styles.iconContainer}>
-            <HelloWave/>
-          </ThemedView>
-        )}
-        <ThemedText type='default' style={message.viewed_at ? null : {marginRight: 20}}>
-          {message.title}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.dateRow}>
-        <ThemedText style={styles.dateText}>{message.date || '21 Oct 2017'}</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.descriptionRow}>
-        <ThemedText>{message.description}</ThemedText>
-      </ThemedView>
-      <TouchableOpacity style={styles.readMoreButton}>
-        <ThemedText style={styles.readMoreText}>
-          Continue reading
-        </ThemedText>
-      </TouchableOpacity>
-    </ThemedView>
-  );
+const Card = ({message}: { message: Message }) => {
+    const router = useRouter();
+    const [viewed, setViewed] = useState(message.viewed_at !== '');
+    const handlePress = () => {
+        router.push(`message/${message.id}`);
+        setViewed(true);
+
+    };
+    return (
+        <Pressable onPress={handlePress}>
+            <ThemedView style={styles.container}>
+                <ThemedView style={styles.titleRow}>
+                    {viewed ? null : (
+                        <ThemedView style={styles.iconContainer}>
+                            <HelloWave/>
+                        </ThemedView>
+                    )}
+                    <ThemedText type='default' style={viewed ? null : {marginRight: 20}}>
+                        {message.title}
+                    </ThemedText>
+                </ThemedView>
+                <ThemedView style={styles.dateRow}>
+                    <ThemedText style={styles.dateText}>{message.date || '21 Oct 2017'}</ThemedText>
+                </ThemedView>
+                <ThemedView style={styles.descriptionRow}>
+                    <ThemedText>{message.description}</ThemedText>
+                </ThemedView>
+                <TouchableOpacity style={styles.readMoreButton}>
+                    <ThemedText style={styles.readMoreText} onPress={handlePress}>
+                        Continue reading
+                    </ThemedText>
+                </TouchableOpacity>
+            </ThemedView>
+        </Pressable>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 10,
-  },
-  titleRow: {
-    flexDirection: 'row',
-    marginTop: 5,
-  },
-  iconContainer: {
-    padding: 5,
-  },
-  dateRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 5,
-  },
-  dateText: {
-    fontSize: 12,
-    fontWeight: '300',
-  },
-  descriptionRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 5,
-    flex: 1,
-  },
-  readMoreButton: {
-    marginTop: 5,
-  },
-  readMoreText: {
-    color: '#059669',
-    fontWeight: '600',
-  },
+    container: {
+        padding: 10,
+    },
+    titleRow: {
+        flexDirection: 'row',
+        marginTop: 5,
+    },
+    iconContainer: {
+        padding: 5,
+    },
+    dateRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5,
+    },
+    dateText: {
+        fontSize: 12,
+        fontWeight: '300',
+    },
+    descriptionRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 5,
+    },
+    readMoreButton: {
+        marginTop: 5,
+    },
+    readMoreText: {
+        color: '#059669',
+        fontWeight: '600',
+    },
 });
 
 export default Card;
